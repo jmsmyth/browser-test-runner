8fd87529537a7405f0d586e8ca78df032e9fec47
James Smyth <jmsmyth21@gmail.com>
2017-01-30 22:20:23 +0000

    Fix broken command line tool

--- bin/cli.js
+++ bin/cli.js
@@ -1,16 +1,2 @@
-"use strict"
-
 #!/usr/bin/env node
-if (process.argv[2] === 'watch') {
-  const server = require('../server')
-  server.start()
-} else if (process.argv[2] === 'saucelabs') {
-  const runner = require('../saucelabs')
-  runner({ urlPath: process.argv[3] })
-} else if (process.argv[2] === 'phantomjs') {
-  const runner = require('../phantom')
-  runner({ urlPath: process.argv[3] })
-} else {
-  const runner = require('../browser')
-  runner({ browserName: process.argv[2], urlPath: process.argv[3] })
-}
+require('../cli')()
